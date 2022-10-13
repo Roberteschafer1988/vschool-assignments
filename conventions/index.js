91bf6ef60cf93c379f5c92da5bf7bb68df2b062d
Robert Schafer <Roberteschafer1988@gmail.com>
2022-10-12 21:00:11 -0500

    daily planet editor assignment finished upload

---
+++ conventions/index.js
@@ -0,0 +1,50 @@
+// JS Conventions
+
+// casing -camelCased
+
+var myLocationIsInMinnesota = true
+
+// white spacing
+
+for(var i = 0; i < 10; i++){
+
+}
+
+// naming
+
+var firstName = "robert"
+var lastName = "schafer"
+var friends = []
+var isDone = true
+var isGameOver = false
+
+function sum(num1, num2){
+
+}
+
+// braces
+var person {
+    address: {
+        street: "123 street"
+    }
+}
+
+// tab (indenting)
+if(2 === 2){
+        console.log("they are equal")
+        if(3 === 3){
+            console.log("they are equal")
+        }
+}
+
+// HTML tags
+    // <div> 
+
+// comments
+    /* anything in between is commented
+
+    asdnla
+    */
+
+//unix casing
+//no spaces all lowerspace use a / or _ to seperate words
